4c3271e382abda991e9a070c3e4d2b70e98eb5ae
Aaron Siegler <aaron.siegler@rover.com>
2023-12-08 15:15:14 -0500

    day05 just solve a quadratic equation

--- day06.py
+++ day06.py
@@ -1,4 +1,6 @@
+import math
 import re
+from typing import Tuple
 
 SAMPLE_INPUT = """\
 Time:      7  15   30
@@ -10,6 +12,47 @@ def line_numbers(line: str) -> list[int]:
     return [int(n) for n in re.findall(r"\d+", line)]
 
 
+def find_min_max_naive(time: int, dist: int) -> Tuple[int, int]:
+    winners: list[int] = []
+    for r in range(0, time):
+        d = r * (time - r)
+        if d >= dist:
+            winners.append(r)
+    return (winners[0], winners[-1])
+
+
+def find_min_max_only(time: int, dist: int) -> Tuple[int, int]:
+    winners: list[int] = []
+    for r in range(0, time):
+        d = r * (time - r)
+        if d >= dist:
+            winners.append(r)
+            break
+    for r in range(time, 0, -1):
+        d = r * (time - r)
+        if d >= dist:
+            winners.append(r)
+            break
+    return winners[0], winners[1]
+
+
+def roots(a: int, b: int, c: int) -> list[float]:
+    """Roots of ax^2 + bx + c = 0"""
+    x1 = (-b + math.sqrt(b**2 - 4 * a * c)) / (2 * a)
+    x2 = (-b - math.sqrt(b**2 - 4 * a * c)) / (2 * a)
+    return [x1, x2]
+
+
+def find_min_max_calc(time: int, dist: int) -> Tuple[int, int]:
+    """
+    The distance traveled is described by `dist = (time - x) * x`, which is
+    a quadtratic equation, so we can just calculate the roots to solve it.
+    """
+    winners = []
+    winners = roots(-1, time, -dist)
+    return math.floor(winners[0]), math.floor(winners[1])
+
+
 # hold down button
 # boat speed = 0mm/ms
 # each ms you hold down the button, the speed increases by 1mm/ms
@@ -19,7 +62,7 @@ def solve1(lines: list[str]) -> int:
     # print(races)
     product = 1
     for time, dist in races:
-        winners = []
+        winners: list[Tuple[int, int]] = []
         # we don't have to iterate through every number
         # we could just find the min and max
         # or there is probably just a way to compute it
@@ -36,12 +79,8 @@ def solve2(lines: list[str]) -> int:
     lines = [re.sub(r" ", "", line) for line in lines]
     time, dist = next(zip(line_numbers(lines[0]), line_numbers(lines[1])))
 
-    total = 0
-    for t in range(0, time):
-        d = t * (time - t)
-        if d > dist:
-            total += 1
-    return total
+    x1, x2 = find_min_max_calc(time, dist)
+    return x2 - x1
 
 
 def main():
